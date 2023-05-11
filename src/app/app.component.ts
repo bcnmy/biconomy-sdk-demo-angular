cce6a1744e585dcb3a8f2d4796586eae92209f9c
shaansundar <shaan.idtindia@gmail.com>
2023-05-11 14:16:07 +0530

    feat: social login popup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import SocialLogin from '@biconomy/web3-auth';
+import { ethers } from 'ethers';
+// import "./style.css"
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bsdk-demo-ng';
+  async connectWallet() {
+    // create an instance of SocialLogin
+    const socialLogin = new SocialLogin();
+    // init social login SDK, all params are optional
+    await socialLogin.init();
+
+    // pops up the UI widget
+    socialLogin.showWallet();
+    if (!socialLogin?.provider) return;
+    // create a provider from the social login provider that
+    // will be used by the smart account package of the Biconomy SDK
+    const provider = new ethers.providers.Web3Provider(socialLogin.provider);
+    // get a list of accounts available with the provider
+    const accounts = await provider.listAccounts();
+    console.log('EOA address', accounts);
+    // socialLogin.hideWallet();
+  }
 }
